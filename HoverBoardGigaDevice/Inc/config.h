251d7c4dca1f2e68848fb8c5e51b8a9e5d575c91
David Laban <alsuren@gmail.com>
2021-03-27 16:44:44 +0000

    whitespace changes

--- HoverBoardGigaDevice/Inc/config.h
+++ HoverBoardGigaDevice/Inc/config.h
@@ -41,7 +41,7 @@
 // ################################################################################
 
 #define PWM_FREQ         		16000     // PWM frequency in Hz
-#define DEAD_TIME        		60        // PWM deadtime (60 = 1µs, measured by oscilloscope)
+#define DEAD_TIME        		60        // PWM deadtime (60 = 1ï¿½s, measured by oscilloscope)
 
 #define DC_CUR_LIMIT     		15        // Motor DC current limit in amps
 
